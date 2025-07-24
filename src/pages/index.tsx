f6a979b6e41fe3b96e3ce8317d4bca92fada454e
Ramanpreet Aujla <ramanpreetaujla@Ramanpreets-Mac-mini.local>
2025-07-24 17:40:39 -0400

    Setting up the board

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,31 +13,69 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   const [sudukoSize, setSudukoSize] = useState(9);
+  const [boardValue, setBoardValue] = useState<Array<Array<number>>>([[]]);
+  const [solution, setSolution] = useState<Array<Array<number>>>([[]]);
 
+  const generateSuduko = async () => {
+    const response = await fetch('https://sudoku-api.vercel.app/api/dosuku');
+    const jsonResponse = (await response.json()) as {
+      newboard: {
+        grids: Array<{
+          value: Array<Array<number>>;
+          solution: Array<Array<number>>;
+          difficulty: string;
+        }>
+        message: string;
+        results: number
+      }
+    };
+
+    setBoardValue(jsonResponse.newboard.grids[0].value)
+    setSolution(jsonResponse.newboard.grids[0].solution)
+  }
+
+  const boardChanged = (value: string, row: number, col: number) => {
+    const numberValue = parseInt(value, 10);
+    if (solution[row][col] === numberValue) {
+      // Do nothing
+    } else {
+      // color the text for the input red
+    }
+  }
+
+  useEffect(() => {
+    generateSuduko();
+  }, [])
 
 
   return (
-    <div className="flex justify-center items-center h-screen w-screen">
+    <div className="flex flex-col justify-center items-center h-screen w-screen">
       <div className="suduko grid grid-cols-9 gap-0">
         {
-          [...Array(sudukoSize * sudukoSize)].map((_, index) => {
-            const row = Math.floor(index / sudukoSize);
-            const col = index % sudukoSize;
-
-            return (
-              <input
-                key={index}
-                type="text"
-                value={index}
-                className="text-center border"
-                maxLength={1}
-                data-row={row}
-                data-col={col}
-              />
-            )
+          boardValue.map((row, rowIndex) => {
+            return row.map((cell, colIndex) => {
+              console.log('cel;', cell);
+              return (
+                <input
+                  key={`${rowIndex}-${colIndex}`}
+                  type="text"
+                  onChange={(e) => boardChanged(e.target.value, rowIndex, colIndex)}
+                  value={cell.toString()}
+                  className="text-center border"
+                  maxLength={1}
+                />
+              )
+            })
           })
         }
+
       </div>
+
+
+      <button
+        className="m-5 py-2 px-4 border rounded bg-foreground/20 text-xs cursor-pointer"
+        onClick={generateSuduko}
+      >Generate New Board!</button>
     </div>
 
   );
